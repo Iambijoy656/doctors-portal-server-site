fbbbec3284151c9bafe58ce09d60d4b9a35e6d1b
Bijoy Chandro Das <freelancing.bijoy@gmail.com>
2022-11-19 01:09:50 +0600

    react query

--- index.js
+++ index.js
@@ -29,6 +29,7 @@ async function run() {
             console.log(date)
             const query = {};
             const options = await appointmentOptionCollection.find(query).toArray();
+            // get the bookings of  the provided date
             const bookingQuery = { appointmentDate: date }
             const alreadyBooked = await bookingsCollection.find(bookingQuery).toArray();
 
@@ -38,13 +39,70 @@ async function run() {
                 const optionBooked = alreadyBooked.filter(book => book.treatment === option.name)
 
                 const bookedSlots = optionBooked.map(book => book.slot)
-                console.log(date, option.name, bookedSlots);
+
+                const remainingSlots = option.slots.filter(slot => !bookedSlots.includes(slot))
+
+                option.slots = remainingSlots
+
             })
 
 
             res.send(options)
 
         })
+
+        app.get('/v2/appointmentOptions', async (req, res) => {
+            const date = req.query.date;
+            const options = await appointmentOptionCollection.aggregate([
+                {
+                    $lookup: {
+                        from: 'bookings',
+                        localField: 'name',
+                        foreignField: 'treatment',
+                        pipeline: [
+                            {
+                                $match: {
+                                    $expr: {
+                                        $eq: ['$appointmentDate', date]
+                                    }
+                                }
+                            }
+                        ],
+                        as: 'booked'
+                    }
+                },
+                {
+                    $project: {
+                        name: 1,
+                        slots: 1,
+                        booked: {
+                            $map: {
+                                input: '$booked',
+                                as: 'book',
+                                in: '$$book.slot'
+                            }
+                        }
+                    }
+                },
+                {
+                    $project: {
+                        name: 1,
+                        slots: {
+                            $setDifference: ['$slots', '$booked']
+                        }
+                    }
+                }
+            ]).toArray();
+            res.send(options);
+        })
+
+
+
+
+
+
+
+
 
         /**
          * API naming Convention
@@ -58,6 +116,21 @@ async function run() {
 
         app.post('/bookings', async (req, res) => {
             const booking = req.body
+            console.log(booking)
+            const query = {
+                appointmentDate: booking.appointmentDate,
+                email: booking.email,
+                treatment: booking.treatment,
+
+            }
+            const alreadyBooked = await bookingsCollection.find(query).toArray();
+
+            if (alreadyBooked.length) {
+                const message = `You already have a booking on ${booking.appointmentDate}`
+                return res.send({ acknowledge: false, message });
+            }
+
+
             const result = await bookingsCollection.insertOne(booking)
             res.send(result)
         })
